ada4c268b7e3ba8a2e8f566c2a5d82b818620e60
Tatiana <ganzhinovat@yandex.ru>
2022-05-18 21:22:41 +0300

    feat: add the solution of 04-task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,49 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+const pathFromCopyFolder = path.join(__dirname, 'files');
+const pathToCopyFolder = path.join(__dirname, 'files-copy');
+
+fs.access(pathToCopyFolder, (error) => {
+  if (error) {
+    fsPromises.mkdir(pathToCopyFolder);
+    console.log('Папка files-copy создана');
+  } else {
+    console.log('Папка files-copy уже существует');
+  }
+});
+
+function createCopyFile(pathWhere, content) {
+  fs.writeFile(pathWhere, content, (error) => {
+    if (error) throw error;
+  });
+}
+
+async function copyDir(fromPath, toPath) {
+  await fsPromises.rm(toPath, { force: true, recursive: true });
+  await fsPromises.mkdir(toPath, { recursive: true });
+
+  const filesNameArr = await fsPromises.readdir(fromPath, { withFileTypes: true });
+
+  for (let item of filesNameArr) {
+    const currentItemPath = path.join(fromPath, item.name);
+    const copyItemPath = path.join(toPath, item.name);
+
+    if (item.isDirectory()) {
+      await fsPromises.mkdir(copyItemPath, { recursive: true });
+      await copyDir(currentItemPath, copyItemPath);
+    } else if (item.isFile()) {
+      const rs = fs.createReadStream(currentItemPath);
+
+      let data = '';
+      rs.on('data', chunk => data += chunk);
+      rs.on('end', () => createCopyFile(copyItemPath, data));
+      rs.on('error', err => console.log('Erroооr: ', err.message));
+    }
+  }
+}
+
+copyDir(pathFromCopyFolder, pathToCopyFolder);
+
 
