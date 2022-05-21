a020059e59556da22c1e3642b945bca2342227ab
Tatiana <ganzhinovat@yandex.ru>
2022-05-21 11:04:42 +0300

    refactor: change variable declaration in 04-task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const fsPromises = require('fs/promises');
+const fsPromises = fs.promises;
 const path = require('path');
 
 const pathFromCopyFolder = path.join(__dirname, 'files');
